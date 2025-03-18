67c38251c17508b676fa9b4c60940bff33341319
Shankapotamus88 <bradley.w.maxwell@gmail.com>
2025-03-17 20:50:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-// Function to toggle the dropdown menu
-function toggleMenu() {
-    let menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
-
-// Close menu when clicking outside of it
-document.addEventListener("click", function(event) {
-    let menu = document.getElementById("menu");
-    let button = document.querySelector(".menu-button");
-    
-    if (!button.contains(event.target) && !menu.contains(event.target)) {
-        menu.style.display = "none";
-    }
-});
+// Currently no interactive functionality is needed.
+console.log("Script loaded");
